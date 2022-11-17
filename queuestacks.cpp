e9d3836e4f273029b74efb91ef5a116ade1796fc
Alex Liu <alexaa543216@gmail.com>
2022-11-17 18:51:42 -0500

    Created read input

--- queuestacks.cpp
+++ queuestacks.cpp
@@ -2,5 +2,55 @@
 
 #include <iostream>
 #include <vector>
+#include <string>
+#include <sstream>
+#include <fstream>
 
 using namespace std;
+
+class Simple_list{
+    string name;
+    public:
+        virtual void push();
+        virtual void pop();
+    protected:
+        int insert_end();
+        int intsert_start();
+        int remove();
+};
+void read_input(string filein, string fileout, vector<Simple_list*> &vector){
+    string line;
+    ifstream myfile(filein);
+    ofstream output(fileout);
+    if(myfile.is_open() && output.is_open()){
+        while(getline(myfile,line) ){
+            string name, command, type, value, stackqueue;
+            stringstream stream;
+            
+            stream << line;
+
+            output << "PROCESSING COMMAND: " << line << " \n";
+
+            stream >> command >> name;
+            
+            if(command.compare("pop") == 0){ //Pop
+            } else if(command.compare("push")){
+                stream >> value;
+            } else {
+                stream >> stackqueue;
+            }
+        }
+        myfile.close();
+        output.close();
+    }
+}
+
+int main(){
+    string in, out;
+    cout << "Enter the input file: ";
+    cin >> in;
+    cout << "Enter the output file: ";
+    cin >> out;
+    vector<Simple_list*> vector;
+    read_input(in, out, vector);
+}
